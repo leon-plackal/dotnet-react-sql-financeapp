235e88a7401c2aa3d32d049e864682530267b709
moon-dweller <155437116+moon-dweller@users.noreply.github.com>
2024-12-15 12:59:24 +1100

    Use repository code to stock controller

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -35,7 +35,7 @@ namespace api.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var stock = await _context.Stocks.FindAsync(id);
+            var stock = await _stockRepo.GetByIdAsync(id);
             if (stock == null)
             {
                 return NotFound();
@@ -47,8 +47,7 @@ namespace api.Controllers
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
             var stockModel = stockDto.ToModelFromDto();
-            await _context.Stocks.AddAsync(stockModel);
-            await _context.SaveChangesAsync();
+            await _stockRepo.CreateAsync(stockModel);
             // get the ID of the newly created stock 
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToDto());
         }
@@ -57,18 +56,12 @@ namespace api.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockDto updateDto)
         {
-            var stock = await _context.Stocks.FindAsync(id);
+            var stock = await _stockRepo.UpdateAsync(id, updateDto);
             if (stock == null)
             {
                 return NotFound();
             }
-            stock.Symbol = updateDto.Symbol;
-            stock.CompanyName = updateDto.CompanyName;
-            stock.Purchase = updateDto.Purchase;
-            stock.LastDiv = updateDto.LastDiv;
-            stock.Industry = updateDto.Industry;
-            stock.MarketCap = updateDto.MarketCap;
-            await _context.SaveChangesAsync();
+
             return Ok(stock.ToDto());
         }
 
@@ -76,13 +69,12 @@ namespace api.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var stock = await _context.Stocks.FindAsync(id);
+            var stock = await _stockRepo.DeleteAsync(id);
             if (stock == null)
             {
                 return NotFound();
             }
-            _context.Stocks.Remove(stock);
-            await _context.SaveChangesAsync();
+            
             return NoContent();
         }
     }
